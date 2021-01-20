1bddd3b3026ed59a603a5c986586df46ed0b1134
dave86dev <davidochando1986@gmail.com>
2021-01-20 16:33:04 +0100

    Documentación y comentario básico añadido a style.css e index.html

---
+++ js/main.js
@@ -0,0 +1,9 @@
+
+//Comentarios de una línea en JavaScript
+
+/* Comentarios multilínea en JavaScript, útiles
+para bloques de comentarios extensos de texto */
+
+
+//Mensaje que aparecerá en Console (F12 en nuestro navegador)
+console.log("Hola Mundo");
